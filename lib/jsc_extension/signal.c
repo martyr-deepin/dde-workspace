e29fe0e3dadd25109417a0e7d441f1c6ffbad730
snyh <snyh@snyh.org>
2012-10-26 11:19:37 +0800

    app:doc add tasklist feature

--- lib/jsc_extension/signal.c
+++ lib/jsc_extension/signal.c
@@ -3,17 +3,25 @@
 
 GHashTable* signals = NULL;
 
-void js_post_message(const char* name, const char* msg_json)
+void js_post_message(const char* name, const char* format, ...)
 {
     if (signals == NULL) {
         g_warning("signals has not init!\n");
         return;
     }
+
+    va_list args;
+    va_start(args, format);
+    char* json_str = g_strdup_vprintf(format, args);
+    va_end(args);
+
+
     JSContextRef ctx = get_global_context();
     JSObjectRef cb = g_hash_table_lookup(signals, name);
     if (cb != NULL) {
         JSValueRef args[1];
-        args[0] = json_from_cstr(ctx, msg_json);
+        args[0] = json_from_cstr(ctx, json_str);
+        g_free(json_str);
         JSObjectCallAsFunction(ctx, cb, NULL, 1, args, NULL);
     } else {
         g_warning("signal %s has not connected!\n", name);
