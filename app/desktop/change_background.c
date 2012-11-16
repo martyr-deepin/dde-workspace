e5308808f43c6c408cf93d3a1eba5bba981c3d33
Hu Kang <hukang@linuxdeepin.com>
2012-11-16 15:01:07 +0800

    add support for crossfading background

--- app/desktop/change_background.c
+++ app/desktop/change_background.c
@@ -30,61 +30,169 @@
 
 #define BG_SCHEMA_ID "org.gnome.desktop.background"
 #define BG_IMAGE_KEY "picture-uri"
+
+
+#define USEC_PER_SEC 1000000.0 // microseconds per second 
+#define MSEC_PER_SEC 1000.0    // milliseconds per second 
+//TODO: get these values from gsettings.
+#define BG_INTERVAL 0.05 //in seconds 
+#define BG_DURATION 1.0 //in seconds
+
+static const gchar* bg_props[2] = {"_XROOTPMAP_ID","ESETROOT_PMAP_ID"};
+static Atom bg1_atom;  
+static Atom bg2_atom;
+static Atom pixmap_atom; 
+
+static Display* display;
+static Window	root;
+static int	default_screen;
+static int	root_depth;
+static Visual*	root_visual;
+static int	root_width;
+static int	root_height;
+
+//
+static gboolean	initialized = FALSE;
+//
+
 /*
- *	@pb : pixbuf for new background, we should ensure we 
- *	      don't pass a invalid pb. 	
+ * 	all the time are in seconds.
  */
-static void set_bg_props(GdkPixbuf* pb, Display* d)
+typedef struct _xfade_data
 {
-	//don't remove following comments:
-	//keep pixmap resource available
-	//XSetCloseDownMode(d,RetainPermanent);
+	//all in seconds.
+	gdouble		 start_time;
+	gdouble		 total_duration;
+	gdouble		 interval;
 	
-	g_assert(pb!=NULL);
+	cairo_surface_t* fading_surface;
+	GdkPixbuf* 	 end_pixbuf;
+	gdouble		 alpha;
 
-	Window root = DefaultRootWindow(d);
-	int screen = DefaultScreen(d);
-	int depth = DefaultDepth(d,screen);
-	Visual* v = DefaultVisual(d,screen);
-	int width = DisplayWidth(d,screen);
-	int height = DisplayHeight(d,screen);
-	
-	Pixmap pm = XCreatePixmap(d,root,width,height,depth);
+	Pixmap		pixmap;
+}xfade_data_t;
 
-	//create cairo surface for this pixmap.
-	cairo_surface_t* cs=NULL;
-	cs = cairo_xlib_surface_create(d,pm,v,width,height);
-	if(cs==NULL)
-	{	
-		XFreePixmap(d,pm);
-		g_object_unref(pb);
-		return;	
-	}
+/*
+ *	change root window x properties.
+ *	TODO: change set_bg_props or _change_bg_xproperties 
+ *	      to a better name.
+ */
+static void _change_bg_xproperties(Pixmap pm)
+{
+	XChangeProperty(display, root, bg1_atom, pixmap_atom,
+			32, PropModeReplace, (unsigned char*)&pm,1);
+	XChangeProperty(display, root, bg2_atom, pixmap_atom,
+			32, PropModeReplace, (unsigned char*)&pm,1);
+	XFlush(display);
+}
+/*
+ *    compositing two cairo surfaces. 
+ */
+static void draw_background(xfade_data_t* fade_data)
+{
+	cairo_t* cr;
+	cr = cairo_create(fade_data->fading_surface);
 
-	cairo_t* cr = cairo_create(cs);
-	if(cr==NULL)
-	{
-		g_object_unref(pb);
-		cairo_surface_destroy(cs);
-		XFreePixmap(d,pm);
-		return;
-	}
+	gdk_cairo_set_source_pixbuf(cr, fade_data->end_pixbuf, 0, 0);
+	cairo_paint_with_alpha (cr, fade_data->alpha);
 
-	gdk_cairo_set_source_pixbuf(cr,pb,0,0);
+	cairo_destroy (cr);
 
-	cairo_paint(cr);
+	_change_bg_xproperties(fade_data->pixmap);
+}
+        	
+/*
+ * 	free fade_data and its fields.
+ */
+static void free_fade_data(xfade_data_t* fade_data)
+{
+	cairo_surface_destroy(fade_data->fading_surface);
+ 	g_object_unref(fade_data->end_pixbuf);
+	g_free(fade_data);
+}
 
-	//dispose
-	cairo_destroy(cr);
-	g_object_unref(pb);
-	cairo_surface_destroy(cs);
+/*
+ *	return current time in seconds.
+ */
+static gdouble get_current_time (void)
+{
+	double timestamp;
+	GTimeVal now;
 
-	//change root window property.
-	static const gchar* bgprops[2] = {"_XROOTPMAP_ID","ESETROOT_PMAP_ID"};
-	Atom abg1 = gdk_x11_get_xatom_by_name(bgprops[0]);
-	Atom abg2 = gdk_x11_get_xatom_by_name(bgprops[1]);
-        Atom apm = gdk_x11_get_xatom_by_name("PIXMAP");
+	g_get_current_time (&now);
 
+	timestamp = ((USEC_PER_SEC * now.tv_sec) + now.tv_usec) / USEC_PER_SEC;
+
+	return timestamp;
+}
+static gboolean on_tick(gpointer user_data)
+{
+	xfade_data_t* fade_data = (xfade_data_t*)user_data;
+
+	gdouble cur_time;
+	cur_time = get_current_time();
+
+	fade_data->alpha = (cur_time - fade_data->start_time) / fade_data->total_duration;
+	fade_data->alpha = CLAMP (fade_data->alpha, 0.0, 1.0);
+
+	draw_background(fade_data);
+
+	static int i=0;
+	printf("tick %d\n",++i);
+	printf("cur_time : %lf\n", cur_time);
+	printf("alpha	 : %lf\n", fade_data->alpha);
+
+	// 'coz fade_data->alpha is a rough value
+	if(fade_data->alpha >=0.9)
+		return FALSE;
+
+	return TRUE;
+}
+static void on_finished(gpointer user_data)
+{
+	xfade_data_t* fade_data = (xfade_data_t*) user_data;
+
+	fade_data->alpha = 1.0;
+
+	draw_background(fade_data);
+
+	free_fade_data(fade_data);
+	printf("crossfade finished \n");
+}
+/*
+ * 	setup cross fade timer and callback.
+ */
+static void crossfade_start(xfade_data_t* fade_data)
+{
+	fade_data->start_time = get_current_time(); 
+	printf("start_time : %lf\n", fade_data->start_time);
+	printf("interval   : %lf\n", fade_data->interval);
+	GSource* source = g_timeout_source_new(fade_data->interval*MSEC_PER_SEC);
+
+	g_source_set_callback(source, (GSourceFunc) on_tick, fade_data, (GDestroyNotify)on_finished);
+	g_source_attach(source, g_main_context_default());
+}
+/*
+ * 	get previous background pixmap id.
+ * 	NOTE: return value:
+ * 	1. None
+ * 	   1) the property is not set.
+ * 	      when we first startup.
+ * 	   2) a pixmap that has been freed
+ * 	   3) a pixmap that is inconsistent 
+ * 	      with current resolution. so 
+ * 	      we need to regenerate a pixmap. 
+ * 	2. non-None 
+ * 	   a valid pixmap that has the same size as 
+ * 	   the root window in pixel.
+ *
+ * 	all None return value indicates that 
+ * 	we need to regenerate a pixmap.
+ *
+ */
+static Pixmap get_previous_background()
+{
+	printf(" enter get_previous_background:\n");
 	//get previous properties.
 	gulong nitems = 0;
 
@@ -92,13 +200,13 @@ static void set_bg_props(GdkPixbuf* pb, Display* d)
 	Pixmap pbg2 = None;
 
 	guchar* prop = NULL;
-	prop = get_window_property(d, root, abg1, &nitems);
+	prop = get_window_property(display, root, bg1_atom, &nitems);
 	if(prop)
 	{
 		pbg1 = X_FETCH_32(prop,0);
 		g_free(prop);
 	}
-	prop = get_window_property(d, root, abg1, &nitems);
+	prop = get_window_property(display, root, bg2_atom, &nitems);
 	if(prop)
 	{
 		pbg2 = X_FETCH_32(prop,0);
@@ -107,15 +215,102 @@ static void set_bg_props(GdkPixbuf* pb, Display* d)
 	//compare two pixmaps.
 	g_assert(pbg1==pbg2);
 
-	//free pixmaps
-	XFreePixmap(d,pbg1);
+	//check whether the pixmap exists
+	Window _root;
+	int _x,_y;
+	unsigned int _width,_height;
+	unsigned int _border,_depth;
+	/*
+	 * 	TODO: how to reliably check the existence of a pixmap.
+	 */
 	
-	//set new background pixmaps.
-	//use XA_PIXMAP instead.
-	XChangeProperty(d,root,abg1,apm,32,PropModeReplace,(unsigned char*)&pm,1);
-	XChangeProperty(d,root,abg2,apm,32,PropModeReplace,(unsigned char*)&pm,1);
+	if(pbg1!=None)
+	{
+		gdk_error_trap_push();
+		Status _s = XGetGeometry(display, pbg1, &_root,
+		                &_x,&_y,&_width,&_height,
+		                &_border,&_depth);
+		if((_s==0)||(_width!=root_width)||(_height!=root_height))
+		{
+			// the drawable have been freed or resolution changed.
+			pbg1 = None;
+		}
+		gdk_error_trap_pop();
+	}
 
-	XFlush(d);
+	printf("prev_pixmap = 0x%x\n", (unsigned) pbg1);
+	printf("leaving get_previous_background:\n");
+	return pbg1;
+}
+
+/*
+ * 	create a cairo surface from a pixmap. this is where 
+ * 	we're drawing on
+ * 	NOTE: @pixmap should not be None.
+ * 	TODO: we assume that @pixmap is the same size as the
+ * 	      root_window. if that's not tree, scale it.
+ */
+static cairo_surface_t* get_surface(Pixmap pixmap)
+{
+	cairo_surface_t* cs=NULL;
+	cs = cairo_xlib_surface_create(display, pixmap, 
+			               root_visual, 
+				       root_width, root_height);
+	
+	return cs;
+}
+/*
+ *	@pb : pixbuf for new background, we should ensure 
+ *	      that pb is not null.
+ */
+static void set_bg_props(GdkPixbuf* pb)
+{
+	//don't remove following comments:
+	//to keep pixmap resource available
+	XSetCloseDownMode(display, RetainPermanent);
+	g_assert(pb!=NULL);
+
+	xfade_data_t* fade_data = g_new0(xfade_data_t, 1);
+
+	Pixmap prev_pixmap = get_previous_background();
+	
+	if(initialized&&(prev_pixmap!=None))
+	{
+		// cross fade 
+		//TODO: scale prev_pixmap to root window size.
+		fade_data->pixmap = prev_pixmap;
+		fade_data->fading_surface = get_surface(prev_pixmap);
+		fade_data->end_pixbuf = pb;
+		//TODO: get this from gsettings.
+		fade_data->total_duration = BG_DURATION;
+		fade_data->interval = BG_INTERVAL;
+		
+		printf("we've setup fade_data\n");
+
+		crossfade_start(fade_data);
+		// free fade_data in on_finished
+		// reuse pixman. no need to change background xproperty.
+		// if this causes problems, we need to set a new Pixmap.
+	}
+	else
+	{
+		//no previous background, no cross fade effect.
+		//this is most likely the situation when we first start up.
+		//resolution changed.
+		Pixmap new_pixmap = XCreatePixmap(display, root, 
+				          root_width,root_height,
+					  root_depth);
+
+		fade_data->pixmap = new_pixmap;
+		fade_data->fading_surface = get_surface(new_pixmap);
+		fade_data->end_pixbuf = pb;
+		fade_data->alpha = 1.0;     
+
+		draw_background(fade_data);
+		free_fade_data(fade_data);
+
+		initialized = TRUE;
+	}
 }
 
 static GdkPixbuf* get_bg_pixbuf_from_gsettings(GSettings* settings)
@@ -127,13 +322,15 @@ static GdkPixbuf* get_bg_pixbuf_from_gsettings(GSettings* settings)
 
 	//creat pixbuf from image file
 	GdkPixbuf* pb = gdk_pixbuf_new_from_file(bg_image, NULL);
+
+	printf("background image: %s\n", bg_image);
 	
 	g_free(bg_image);
 
 	return pb;
 }
 
-static void bg_changed(GSettings *settings, gchar* key, Display* _dsp)
+static void bg_changed(GSettings *settings, gchar* key, gpointer user_data)
 {
 	if (g_strcmp0(key,BG_IMAGE_KEY))
 		return;
@@ -142,21 +339,53 @@ static void bg_changed(GSettings *settings, gchar* key, Display* _dsp)
 	if(pb==NULL)
 		return;
 
-	set_bg_props(pb, _dsp);
+	set_bg_props(pb);
 
 	return;
 }
 
+static void screen_size_changed(GdkScreen* screen, gpointer user_data)
+{
+	root_width = gdk_screen_get_width(screen);
+	root_height = gdk_screen_get_height(screen);
+
+	//TODO: change root bg xproperty.
+	
+	GSettings* settings = (GSettings*) user_data;
+	GdkPixbuf* pb = get_bg_pixbuf_from_gsettings(settings);
+	if(pb==NULL)
+		return;
+	set_bg_props(pb);
+}
+
 void install_background_handler(void)
 {
-	Display* _dsp = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+	display = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+
+	bg1_atom = gdk_x11_get_xatom_by_name(bg_props[0]);
+	bg2_atom = gdk_x11_get_xatom_by_name(bg_props[1]);
+	pixmap_atom = gdk_x11_get_xatom_by_name("PIXMAP");
+
+	root = DefaultRootWindow(display);
+	default_screen = DefaultScreen(display);
+	root_depth = DefaultDepth(display, default_screen);
+	root_visual = DefaultVisual(display, default_screen);
+	root_width = DisplayWidth(display, default_screen);
+	root_height = DisplayHeight(display, default_screen);
+
+	initialized = FALSE;
+
+	GdkScreen* gdk_screen = gdk_screen_get_default();
 
 	GSettings* bg_setting = g_settings_new (BG_SCHEMA_ID);
-	g_signal_connect(bg_setting,"changed", G_CALLBACK(bg_changed), _dsp);
+
+	g_signal_connect(bg_setting,"changed", G_CALLBACK(bg_changed), NULL);
+	g_signal_connect(gdk_screen,"size-changed", G_CALLBACK(screen_size_changed), bg_setting);
 
 	GdkPixbuf* pb = get_bg_pixbuf_from_gsettings(bg_setting);
 	if(pb==NULL) //just return.
 		return;
 
-	set_bg_props(pb, _dsp);
+	set_bg_props(pb);
+
 }
