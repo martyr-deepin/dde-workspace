25e5fe178d96a16b8f4dce1db62e2242b3752ab9
yilang2007lw <yilang2007lw@gmail.com>
2012-12-21 17:11:37 +0800

    app:let greeter compile pass

--- app/greeter/greeter.c
+++ app/greeter/greeter.c
@@ -220,42 +220,8 @@ gboolean greeter_shutdown()
     return lightdm_shutdown(NULL);
 }
 
-
 int main(int argc, char **argv)
 {
-    GMainLoop *main_loop;
-    LightDMGreeter *greeter;
-    main_loop = g_main_loop_new ();
-    greeter = lightdm_greeter_new ();
-
-    g_object_connect (greeter, "show-prompt", G_CALLBACK (show_prompt_cb), NULL);
-    g_object_connect (greeter, "authentication-complete", G_CALLBACK (authentication_complete_cb), NULL);
-    // Connect to LightDM daemon
-    if (!lightdm_greeter_connect_sync (greeter, NULL))
-        return EXIT_FAILURE;
-    // Start authentication
-    lightdm_greeter_authenticate (greeter, NULL);
-
-    g_main_loop_run (main_loop);
-
+    printf("Hello Greeter!\n");
     return 0;
-}
-
-static void show_prompt_cb(LightDMGreeter *greeter, const char *text, LightDMPromptType type)
-{
-    // Show the user the message and prompt for some response
-    gchar *secret = prompt_user (text, type);
-    // Give the result to the user
-    lightdm_greeter_respond (greeter, response);
-}
-
-static void authentication_complete_cb(LightDMGreeter *greeter)
-{
-    // Start the session
-    if (!lightdm_greeter_get_is_authenticated (greeter) ||
-        !lightdm_greeter_start_session_sync (greeter, NULL))
-    {
-        // Failed authentication, try again
-        lightdm_greeter_authenticate (greeter, NULL);
-    }
 }
