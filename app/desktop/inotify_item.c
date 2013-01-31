0492b798f73ba807dad041586619aaf96cc88f1d
Hu Kang <hukang@linuxdeepin.com>
2013-01-31 15:34:04 +0800

    desktop.c: free exec_val after using it

--- app/desktop/inotify_item.c
+++ app/desktop/inotify_item.c
@@ -171,8 +171,8 @@ gboolean _inotify_poll()
 
         for (int i=0; i<length; ) {
             struct inotify_event *event = (struct inotify_event *) &buffer[i];
-            if (event->name[0] == '.') continue;
             i += EVENT_SIZE+event->len;
+            if (event->name[0] == '.' && !g_str_has_prefix(event->name, DEEPIN_RICH_DIR)) continue;
             if (event->len) {
                 printf ("%s changed 0x%x\n", event->name, event->mask);
                 GFile* p = g_hash_table_lookup(_monitor_table, GINT_TO_POINTER(event->wd));
