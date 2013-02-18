d21af4be35ebb5e3273652660da3bd5005262a01
Hu Kang <hukang@linuxdeepin.com>
2013-02-18 17:30:16 +0800

    thumbnails: add gfile_can_thumbnail

--- lib/dentry/thumbnails.c
+++ lib/dentry/thumbnails.c
@@ -15,15 +15,9 @@ get_thumbnail_factory ()
     return thumbnail_factory;
 }
 
-/*
- *      syncronously create thumbnails. shall we move to a threaded 
- *      implementation?
- */
-char*
-gfile_lookup_thumbnail (GFile* file)
+gboolean 
+gfile_can_thumbnail (GFile* file)
 {
-    char* thumbnail_path = NULL;
-
     GnomeDesktopThumbnailFactory *factory;
     gboolean can_thumbnail;
     char* uri;
@@ -50,12 +44,52 @@ gfile_lookup_thumbnail (GFile* file)
                                                          mime_type,
                                                          mtime);
     g_debug ("%s can thumbnail(mime: %s): %d", uri, mime_type, can_thumbnail);
+
+    return can_thumbnail;
+}
+/*
+ *      syncronously create thumbnails. shall we move to a threaded 
+ *      implementation?
+ */
+char*
+gfile_lookup_thumbnail (GFile* file)
+{
+    char* thumbnail_path = NULL;
+
+    GnomeDesktopThumbnailFactory *factory;
+    //gboolean can_thumbnail;
+    char* uri;
+    GFileInfo* info;
+    time_t mtime;
+    const char* content_type;
+    char* mime_type;
+		
+    uri = g_file_get_uri (file);
+
+    info = g_file_query_info (file, "standard::content-type,time::modified",
+                              G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
+                              NULL, NULL);
+    content_type = g_file_info_get_content_type (info);
+    mime_type = g_content_type_get_mime_type (content_type);
+
+    mtime = g_file_info_get_attribute_uint64(info, "time::modified");
+    g_object_unref (info);
+	
+    factory = get_thumbnail_factory ();
+#if 0
+    //1' check if we can thumbnail
+    can_thumbnail = gnome_desktop_thumbnail_factory_can_thumbnail (factory,
+                                                         uri,
+                                                         mime_type,
+                                                         mtime);
+    g_debug ("%s can thumbnail(mime: %s): %d", uri, mime_type, can_thumbnail);
     if (can_thumbnail == FALSE)
     {
 	g_free (uri);
 	g_free (mime_type);
 	return NULL;
     }
+#endif
     //2' lookup existing thumbnail
     thumbnail_path = gnome_desktop_thumbnail_factory_lookup (factory, uri, mtime);
     g_debug ("uri: %s\nthumbnail_path: %s\n", uri, thumbnail_path);
--- lib/dentry/thumbnails.h
+++ lib/dentry/thumbnails.h
@@ -1,7 +1,7 @@
 #ifndef _THUMBNAILS_H_
 #define _THUMBNAILS_H_
 
-
-char*   gfile_lookup_thumbnail (GFile* file);
+gboolean gfile_can_thumbnail (GFile* file);
+char*    gfile_lookup_thumbnail (GFile* file);
 
 #endif 
