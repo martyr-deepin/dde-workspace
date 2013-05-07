f5a86540e99458d00cf71e2c6e64573634ecdb46
hk <hk@hk-HP-Compaq-6531s.(none)>
2013-05-07 10:09:43 +0800

    lib/gs-grab.h: remove unused functions

--- lib/gs-grab.h
+++ lib/gs-grab.h
@@ -19,7 +19,6 @@
  * Authors: William Jon McCann <mccann@jhu.edu>
  *
  */
-
 #ifndef __GS_GRAB_H
 #define __GS_GRAB_H
 
@@ -27,7 +26,6 @@
 #include <gdk/gdk.h>
 
 G_BEGIN_DECLS
-
 #define GS_TYPE_GRAB         (gs_grab_get_type ())
 #define GS_GRAB(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GS_TYPE_GRAB, GSGrab))
 #define GS_GRAB_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), GS_TYPE_GRAB, GSGrabClass))
@@ -49,31 +47,11 @@ typedef struct
 
 } GSGrabClass;
 
-GType     gs_grab_get_type         (void);
-
 GSGrab  * gs_grab_new              (void);
-
-void      gs_grab_release          (GSGrab    *grab);
-gboolean  gs_grab_release_mouse    (GSGrab    *grab);
-
-gboolean  gs_grab_grab_window      (GSGrab    *grab,
-                                    GdkWindow *window,
-                                    GdkScreen *screen,
-                                    gboolean   hide_cursor);
-
-gboolean  gs_grab_grab_root        (GSGrab    *grab,
-                                    gboolean   hide_cursor);
-gboolean  gs_grab_grab_offscreen   (GSGrab    *grab,
-                                    gboolean   hide_cursor);
 
 void      gs_grab_move_to_window   (GSGrab    *grab,
                                     GdkWindow *window,
                                     GdkScreen *screen,
                                     gboolean   hide_cursor);
-
-void      gs_grab_mouse_reset      (GSGrab    *grab);
-void      gs_grab_keyboard_reset   (GSGrab    *grab);
-
 G_END_DECLS
-
 #endif /* __GS_GRAB_H */
