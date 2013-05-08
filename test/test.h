f5b8adf23d454065bc00a1805372cd8716f94f4d
kosl90 <kos1990l@gmail.com>
2013-05-08 17:52:36 +0800

    test: define RES_IN_KB/RES_IN_MB/RES_IN_GB to use

--- test/test.h
+++ test/test.h
@@ -9,5 +9,8 @@ gboolean T_test(TestFunc f, gpointer data, int ms, int count);
 gboolean T(TestFunc f, const char* test_name);
 
 #define Test(body, name) T($(body), name)
+#define RES_IN_KB(n) (n)
+#define RES_IN_MB(n) (n * 1024)
+#define RES_IN_GB(n) (MB(n) * 1024)
 
 #endif
