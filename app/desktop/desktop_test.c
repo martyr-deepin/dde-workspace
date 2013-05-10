999cc9adcb7058a8c89c774500f592b4e4eb29e7
yuanjq <yuanjq91@gmail.com>
2013-05-10 15:47:13 +0800

    desktop_test: Add some test functions.

--- app/desktop/desktop_test.c
+++ app/desktop/desktop_test.c
@@ -4,6 +4,8 @@
 #include <gtk/gtk.h>
 #include <glib.h>
 #include <X11/X.h>
+#include <JavaScriptCore/JSObjectRef.h>
+#include "jsextension.h"
 #include "test.h"
 #include "background_util.h"
 #include "inotify_item.h"
@@ -186,7 +188,7 @@ void test_background_util()
     GdkPixbuf *get_xformed_gdk_pixbuf(const char *path);
 
     Test({
-        gchar *path = file1;
+        const gchar *path = file1;
         get_xformed_gdk_pixbuf(path);
     }, "get_xformed_gdk_pixbuf");
 
@@ -314,50 +316,178 @@ void test_background_util()
     // parse_picture_uris (gchar * pic_uri)
     void parse_picture_uris (gchar * pic_uri);
     Test({
-        parse_picture_uris(file1);
+        gchar *uri = "~/Desktop/bg.png";
+        parse_picture_uris(uri);
     }, "parse_picture_uris");
 
     GdkPixbuf *get_xformedgdk_pixbuf(const char *path);
     Test({
-        gchar *path = file1;
-        GdkPixbuf *pixbuf = get_xformed_gdk_pixbuf(path);
+        GdkPixbuf *pixbuf = get_xformed_gdk_pixbuf(file1);
         g_object_unref(pixbuf);
     }, "get_xformed_gdk_pixbuf");
  
 }
 
-void test_other()
-{
-}
-
 void test_desktop()
 {
-    /*JSObjectRef desktop_get_desktop_entries();*/
-    /*desktop_get_desktop_entries();*/
+    JSObjectRef desktop_get_desktop_entries();
+    Test({
+        desktop_get_desktop_entries();
+    }, "desktop_get_desktop_entries");
 
-    /*char* desktop_get_rich_dir_name(GFile* dir);*/
-    /*desktop_get_rich_dir_name();*/
+    char* desktop_get_rich_dir_name(GFile* dir);
+    Test({
+        GFile *f = g_file_new_for_path("~/Desktop/.deepin_rich_dir_desktop_test");
+        char *filename = desktop_get_rich_dir_name(f);
+        g_free(filename);
+    }, "desktop_get_rich_dir_name");
 
-    /*void desktop_set_rich_dir_name(GFile* dir, const char* name)*/
-    /*desktop_set_rich_dir_name()*/
-    /*char* desktop_get_rich_dir_icon(GFile* _dir)*/
-    /*GFile* desktop_create_rich_dir(ArrayContainer fs)*/
-    /*char* desktop_get_desktop_path()*/
-    /*GFile* _get_useable_file(const char* basename)*/
-    /*GFile* desktop_new_file()*/
-    /*GFile* desktop_new_directory()*/
-    /*void update_workarea_size(GSettings* dock_gsettings)*/
-    /*void dock_config_changed(GSettings* settings, char* key, gpointer usr_data)*/
-    /*void desktop_config_changed(GSettings* settings, char* key, gpointer usr_data)*/
-    /*gboolean desktop_get_config_boolean(const char* key_name)*/
-    /*void screen_change_size(GdkScreen *screen, GdkWindow *w)*/
-    /*void send_lost_focus()*/
-    /*void send_get_focus()*/
+    void desktop_set_rich_dir_name(GFile* dir, const char* name);
+    Test({
+        GFile *f = g_file_new_for_path("~/Desktop/.deepin_rich_dir_desktop_test");
+        desktop_set_rich_dir_name(f, "test");
+    }, "desktop_set_rich_dir_name");
 
+    char* desktop_get_rich_dir_icon(GFile* _dir);
+    Test({
+        GFile *f = g_file_new_for_path("~/Desktop/.deepin_rich_dir_desktop_test");
+        char *filename = desktop_get_rich_dir_name(f);
+        g_free(filename);
+        g_object_unref(f);
+    }, "desktop_get_rich_dir_icon");
+
+    GFile* desktop_create_rich_dir(ArrayContainer fs);
+    Test({
+        ArrayContainer fs;
+        desktop_create_rich_dir(fs);
+    }, "desktop_create_rich_dir");
+
+    char* desktop_get_desktop_path();
+    Test({
+        char *path = desktop_get_desktop_path();
+        g_free(path);
+    }, "desktop_get_desktop_path");
+
+    GFile* _get_useable_file(const char* basename);
+    Test({
+        GFile *f = _get_useable_file(file1);
+        if(f != NULL)
+            g_object_unref(f);
+    }, "_get_useable_file");
+
+    GFile* desktop_new_file();
+    Test({
+        GFile *f = desktop_new_file();
+        if(NULL != f)
+            g_object_unref(f);
+    }, "desktop_new_file");
+
+    GFile* desktop_new_directory();
+    Test({
+        GFile *f = desktop_new_directory();
+        if (NULL != f)
+            g_object_unref(f);
+    }, "desktop_new_directory");
+    
+    
+    void dock_config_changed(GSettings* settings, char* key, gpointer usr_data);
+    Test({
+        #define DOCK_SCHEMA_ID "com.deepin.dde.dock"
+        GSettings *dock_gsettings = g_settings_new (DOCK_SCHEMA_ID);
+        dock_config_changed(dock_gsettings, "changed::hide-mode", NULL);
+        g_object_unref(dock_gsettings);
+    }, "dock_config_changed");
+
+
+    void desktop_config_changed(GSettings* settings, char* key, gpointer usr_data);
+    Test({
+        #define DESKTOP_SCHEMA_ID "com.deepin.dde.desktop"
+        GSettings *desktop_gsettings = g_settings_new (DESKTOP_SCHEMA_ID);
+        desktop_config_changed(desktop_gsettings, "changed::show-home-icon", NULL);
+        g_object_unref(desktop_gsettings);
+    }, "desktop_config_changed");
+
+    gboolean desktop_get_config_boolean(const char* key_name);
+    Test({
+        desktop_get_config_boolean("changed::show-home-icon");
+    }, "desktop_get_config_boolean");
+
+    void screen_change_size(GdkScreen *screen, GdkWindow *w);
+    Test({
+        extern GtkWidget *container;
+        GdkScreen *screen = gtk_window_get_screen(GTK_WINDOW(container));
+        screen_change_size(screen, gtk_widget_get_window(container));
+        g_object_unref(screen);
+    }, "screen_change_size");
+
+    void send_lost_focus();
+    Test({
+        send_lost_focus();
+    }, "send_lost_focus");
+
+    void send_get_focus();
+    Test({
+        send_get_focus();
+    }, "send_lost_focus");
+
+    void desktop_emit_webview_ok();
+    Test({
+        desktop_emit_webview_ok();
+    }, "desktop_emit_webview_ok");
+
+    // these functio used in other functions.
+    /*void update_workarea_size(GSettings* dock_gsettings);*/
     /*GdkFilterReturn watch_workarea(GdkXEvent *gxevent, GdkEvent* event, gpointer user_data)*/
     /*void unwatch_workarea_changes(GtkWidget* widget)*/
     /*void watch_workarea_changes(GtkWidget* widget, GSettings* dock_gsettings)*/
-    /*void desktop_emit_webview_ok()*/
+}
+
+void test_utils()
+{
+    typedef void Entry;
+
+    void desktop_run_terminal();
+    Test({
+        desktop_run_terminal();
+    }, "desktop_run_terminal");
+
+    void desktop_run_deepin_settings(const char* mod);
+    Test({
+        desktop_run_deepin_settings("display");
+    }, "desktop_run_deepin_settings");
+
+    void desktop_run_deepin_software_center();
+    Test({
+        desktop_run_deepin_software_center();
+    }, "desktop_run_deepin_software_center");
+
+    void desktop_open_trash_can();
+    Test({
+        desktop_open_trash_can();
+    }, "desktop_open_trash_can");
+
+    Entry* desktop_get_home_entry();
+    Test({
+        Entry *entry = desktop_get_home_entry();
+        g_object_unref(entry);
+    }, "desktop_get_home_entry");
+
+    Entry* desktop_get_computer_entry();
+    Test({
+        Entry *entry = desktop_get_computer_entry();
+        g_object_unref(entry);
+    }, "desktop_get_computer_entry");
+
+    char* desktop_get_transient_icon (Entry* p1);
+    Test({
+        GFile *f = g_file_new_for_path(file1); 
+        desktop_get_transient_icon((Entry *)f);
+        g_object_unref(f);
+    }, "desktop_get_transient_icon");
+}
+
+void test_other()
+{
 }
 
 void desktop_test()
@@ -367,13 +497,15 @@ void desktop_test()
     
     //test_dbus();
 
-    //test_background();
+    test_background();
 
-    //test_background_util();
+    test_background_util();
 
-    //test_desktop();
+    test_desktop();
+
+    test_utils();
     
-    //test_other();
+    test_other();
 }
 
 #endif
