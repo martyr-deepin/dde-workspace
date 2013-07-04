6582a9b60a80be2df6e3ac02f4624b6318d8ccdb
bluth653426595 <ycl_pj@163.com>
2013-07-04 17:59:12 +0800

    replace by GPtrArray to give test_variable

--- app/desktop/test/desktop_test_entry.c
+++ app/desktop/test/desktop_test_entry.c
@@ -1,13 +1,13 @@
 #include "desktop_test.h"
-extern void variable_init();
-extern void variable_free();
+extern void setup_fixture();
+extern void tear_down_fixture();
 extern void func_test_entry_char(char* (*func)(Entry*),Entry* variable,char* value_return);
 
 
 gboolean FLAG_PRITN_RESULT = TRUE;
-Entry* gfileDirectory = NULL;
-Entry* gfileDocument = NULL;
-Entry* gappinfo = NULL;
+GPtrArray* gfileDirectory = NULL;
+GPtrArray* gfileDocument = NULL;
+GPtrArray* gappinfo= NULL;
 
 void setup_fixture()
 {
@@ -15,89 +15,100 @@ void setup_fixture()
     // g_creat("test_files/test.c");
     
     system("rm -rf test_files/");
+    // system("rm -rf ahsouifghasdgoiasdghah_sdgfuioashfdiosasdiafohdsoig_ashgioasdhaoisdhoifhasoi_aiosdfhasdoifhasodiufh/");
+    // system("rm -rf _ahdsgioahgaosidg_agioasdhgo/");
+    // system("rm -rf (test_files)/");
+    // system("rm -rf 0ashdgioasdhgo_asdhgio/");
+    // system("rm -rf &asdgasd/");
+
+
     system("mkdir test_files");
+    // system("mkdir ahsouifghasdgoiasdghah_sdgfuioashfdiosasdiafohdsoig_ashgioasdhaoisdhoifhasoi_aiosdfhasdoifhasodiufh");
+    // system("mkdir _ahdsgioahgaosidg_agioasdhgo");
+    // system("mkdir (test_files)");
+    // system("mkdir 0ashdgioasdhgo_asdhgio&asjdgioadsjg");
+    // system("mkdir &asdgasd");
+
 
     system("cp /usr/share/backgrounds/default_background.jpg test_files/");
-
-    system("cp /usr/share/applications/deepin-desktop.desktop test_files/");
-    system("cp /usr/share/applications/deepin-media-player.desktop test_files/");
-    system("cp /usr/share/applications/skype.desktop test_files/");
-    system("cp /usr/share/applications/deepin-system-settings.desktop test_files/");
-    system("cp /usr/share/applications/audacity.desktop test_files/");
-    system("cp /usr/share/applications/brasero.desktop test_files/");
-
-
     system("touch test_files/test.desktop");
     system("touch test_files/test");
-    system("touch test_files/test.c");
     system("touch test_files/test.coffee");
-    system("touch test_files/test.css");
     system("touch test_files/test.cpp");
+    system("touch test_files/test.css");
+    system("touch test_files/test.exe");
     system("touch test_files/test.h");
     system("touch test_files/test.html");
     system("touch test_files/test.js");
     system("touch test_files/test.m");
     system("touch test_files/test.test");
+    system("touch test_files/test.c");
     system("touch test_files/test.txt");
     system("touch test_files/test.wpt");
     system("touch test_files/test.xls");
     system("touch test_files/test.ppt");
     system("touch test_files/test.doc");
-    
+
+    system("cp /usr/share/applications/skype.desktop test_files/");
+    system("cp /usr/share/applications/deepin-desktop.desktop test_files/");
+    system("cp /usr/share/applications/deepin-media-player.desktop test_files/");
+    system("cp /usr/share/applications/deepin-system-settings.desktop test_files/");
+    system("cp /usr/share/applications/audacity.desktop test_files/");
+    system("cp /usr/share/applications/brasero.desktop test_files/");
+
+    gfileDirectory = g_ptr_array_new();
+    gfileDocument = g_ptr_array_new();
+    gappinfo = g_ptr_array_new();
+
+    g_ptr_array_add(gfileDirectory, g_file_new_for_path("test_files"));
+    // g_ptr_array_add(gfileDirectory, g_file_new_for_path("ahsouifghasdgoiasdghah_sdgfuioashfdiosasdiafohdsoig_ashgioasdhaoisdhoifhasoi_aiosdfhasdoifhasodiufh"));
+    // g_ptr_array_add(gfileDirectory, g_file_new_for_path("_ahdsgioahgaosidg_agioasdhgo"));
+    // g_ptr_array_add(gfileDirectory, g_file_new_for_path("(test_files)"));
+    // g_ptr_array_add(gfileDirectory, g_file_new_for_path("0ashdgioasdhgo_asdhgio&asjdgioadsjg"));
+    // g_ptr_array_add(gfileDirectory, g_file_new_for_path("&asdgasd"));
+
+
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/default_background.jpg"));//0
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/skype.desktop"));//1
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text"));//2
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.coffee"));//3
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.cpp"));//4
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.css"));//5
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.exe"));//6
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.h"));//7
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.html"));//8
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.js"));//9
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.m"));//10
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.test"));//11
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.c"));//13
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.txt"));//14
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.wpt"));//15
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.xls"));//16
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.ppt"));//17
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/text.doc"));//18
+    g_ptr_array_add(gfileDocument, g_file_new_for_path("test_files/deepin-system-settings.desktop"));//19
+
+    g_ptr_array_add(gappinfo, g_desktop_app_info_new_from_filename("test_files/skype.desktop"));//0
+    g_ptr_array_add(gappinfo, g_desktop_app_info_new_from_filename("test_files/deepin-desktop.desktop"));//1
+    g_ptr_array_add(gappinfo, g_desktop_app_info_new_from_filename("test_files/deepin-media-player.desktop"));//2
+    g_ptr_array_add(gappinfo, g_desktop_app_info_new_from_filename("test_files/deepin-system-settings.desktop"));//3
+    g_ptr_array_add(gappinfo, g_desktop_app_info_new_from_filename("test_files/audacity.desktop"));//4
+    g_ptr_array_add(gappinfo, g_desktop_app_info_new_from_filename("test_files/brasero.desktop"));//5
+
 }
 
 void tear_down_fixture()
 {
+    g_ptr_array_unref(gfileDirectory);
+    g_ptr_array_unref(gfileDocument);
+    g_ptr_array_unref(gappinfo);
+
     system("rm -rf test_files/");
-}
-
-void variable_init()
-{  
-    setup_fixture();
-
-    gfileDirectory = g_file_new_for_path("/home/ycl/test_files");
-
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/001.png");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/skype.desktop");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.coffee");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.cpp");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.css");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.exe");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.h");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.html");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.js");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.m");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.test");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.m");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.c");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.txt");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.wpt");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.xls");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.ppt");
-    // gfileDocument = g_file_new_for_path("/home/ycl/test_files/text.doc");
-
-    // gappinfo = g_desktop_app_info_new_from_filename("/home/ycl/Desktop/skype.desktop");
-    // gappinfo = g_desktop_app_info_new_from_filename("/home/ycl/test_files/skype.desktop");
-
-
-    gfileDocument = g_file_new_for_path("test_files/default_background.jpg");
-    // gfileDocument = g_file_new_for_path("test_files/skype.desktop");
-    // gappinfo = g_desktop_app_info_new_from_filename("test_files/skype.desktop");
-    gappinfo = g_desktop_app_info_new_from_filename("test_files/audacity.desktop");
-    // 
-
-
-
-}
-
-void variable_free()
-{
-    g_object_unref(gfileDirectory);
-    g_object_unref(gfileDocument);
-    g_object_unref(gappinfo);
-    
-    tear_down_fixture();
+    // system("rm -rf ahsouifghasdgoiasdghah_sdgfuioashfdiosasdiafohdsoig_ashgioasdhaoisdhoifhasoi_aiosdfhasdoifhasodiufh/");
+    // system("rm -rf _ahdsgioahgaosidg_agioasdhgo/");
+    // system("rm -rf (test_files)/");
+    // system("rm -rf 0ashdgioasdhgo_asdhgio/");
+    // system("rm -rf &asdgasd/");
 }
 
 void func_test_entry_char(char* (*func)(Entry*),Entry* variable,char* value_return)
@@ -117,7 +128,7 @@ void func_test_entry_char(char* (*func)(Entry*),Entry* variable,char* value_retu
 
 void test_entry()
 {
-    variable_init();
+    setup_fixture();
 
     // extern Entry* dentry_get_desktop();
     // Test({
@@ -168,7 +179,6 @@ void test_entry()
     //     g_assert(g_str_equal(s, answer) == TRUE);\
     //     g_free(s);\
     // } while(0)
-
     // Test({
     //     func_test_entry_char(dentry_get_uri,gfileDirectory,"file:///home/ycl/Desktop");
     //     // DENTRY_GET_URL_TEST(gfileDirectory, "file:///home/ycl/test_files");
@@ -185,7 +195,6 @@ void test_entry()
     //     g_assert(g_str_equal(s, answer) == TRUE);\
     //     g_free(s);\
     // } while(0)
-
     // Test({
     //     DENTRY_GET_ICON_TEST(gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
     // },"dentry_get_icon");
@@ -198,20 +207,16 @@ void test_entry()
     // },"dentry_get_uri"); 
     
     // extern char* dentry_get_icon(Entry* e);    
-
-    Test({
-        set_default_theme("Deepin");
-        set_desktop_env_name("Deepin");
-        // func_test_entry_char(dentry_get_icon,gfileDirectory, "/usr/share/icons/Faenza/places/48/inode-directory.png");
-        //the icon isn't icon which in .thumbnail/ ,and the file icon show first in thumbnail (code in desktop_item.coffee->set_icon)
-        // func_test_entry_char(dentry_get_icon,gfileDocument, "/usr/share/icons/Faenza/mimetypes/48/application-x-ms-dos-executable.png");
-        // func_test_entry_char(dentry_get_icon,gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
-        // func_test_entry_char(dentry_get_icon,gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
-        func_test_entry_char(dentry_get_icon,gappinfo, "/usr/share/icons/Deepin/apps/48/audacity.png");
-
-        
-        // 
-    },"dentry_get_icon");
+    // Test({
+    //     set_default_theme("Deepin");
+    //     set_desktop_env_name("Deepin");
+    //     // func_test_entry_char(dentry_get_icon,gfileDirectory, "/usr/share/icons/Faenza/places/48/inode-directory.png");
+    //     //the icon isn't icon which in .thumbnail/ ,and the file icon show first in thumbnail (code in desktop_item.coffee->set_icon)
+    //     // func_test_entry_char(dentry_get_icon,gfileDocument, "/usr/share/icons/Faenza/mimetypes/48/application-x-ms-dos-executable.png");
+    //     // func_test_entry_char(dentry_get_icon,gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
+    //     // func_test_entry_char(dentry_get_icon,gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
+    //     func_test_entry_char(dentry_get_icon,gappinfo, "/usr/share/icons/Deepin/apps/48/audacity.png");
+    // },"dentry_get_icon");
 
     // extern char* dentry_get_icon_path(Entry* e);
     // Test({
@@ -240,17 +245,30 @@ void test_entry()
 
     //     // gfileDirectory :dentry_can_thumbnail is false
     //     // func_test_entry_char(dentry_get_thumbnail,gfileDirectory,NULL);
-    //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, "/home/ycl/.thumbnails/normal/692aec2ade9c8ea309697fbd5e9d7899.png");
-    //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, "/home/ycl/.thumbnails/normal/692aec2ade9c8ea309697fbd5e9d7899.png");
+    //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, ".thumbnails/normal/692aec2ade9c8ea309697fbd5e9d7899.png");
+    //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, ".thumbnails/normal/692aec2ade9c8ea309697fbd5e9d7899.png");
 
     //     // .desktop file :dentry_can_thumbnail is false
     //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, NULL);
     //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, NULL);
-    //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, "/home/ycl/.thumbnails/normal/048e2190b2e95ed836646e70e4978378.png");
+    //     // func_test_entry_char(dentry_get_thumbnail,gfileDocument, ".thumbnails/normal/048e2190b2e95ed836646e70e4978378.png");
 
     //     // func_test_entry_char(dentry_get_thumbnail,gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
     //     // func_test_entry_char(dentry_get_thumbnail,gappinfo, "/usr/share/icons/Deepin/apps/48/skype.png");
     // },"dentry_get_thumbnail");
 
-    variable_free();
+
+    extern char* dentry_get_id(Entry* e);
+    Test({
+        func_test_entry_char(dentry_get_id, g_ptr_array_index(gfileDirectory,0),"5565c4012ed4c6f1007c2b86aea48956");
+        // func_test_entry_char(dentry_get_id,gfileDocument,"9e59f7e96e3ca18daa14f5adbbbdcf65");
+        // func_test_entry_char(dentry_get_id,gfileDocument,"221d498715901ee41709b24e36069fed");
+        // func_test_entry_char(dentry_get_id,gfileDocument,"ee38506f63e67c3f1743ca4eeddb1fcb");
+        // func_test_entry_char(dentry_get_id,gappinfo,"e9ab3a2fb814421ed6d747c62851abb3");
+    },"dentry_get_id");
+
+
+
+
+    tear_down_fixture();
 }
