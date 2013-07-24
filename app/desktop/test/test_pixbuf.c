7bf90a1ed3212c00bb534309d027d14222a22831
bluth653426595 <ycl_pj@163.com>
2013-07-24 11:17:08 +0800

    fix bug of pixbuf get_data_uri_by_path didnt g_object_unref(pixbuf)

--- app/desktop/test/test_pixbuf.c
+++ app/desktop/test/test_pixbuf.c
@@ -4,37 +4,38 @@ void test_pixbuf()
 	// setup_fixture();4
 
 	char* generate_directory_icon(const char* p1, const char* p2, const char* p3, const char* p4);
-	char* get_data_uri_by_path(const char* path);
-	char* get_data_uri_by_pixbuf(GdkPixbuf* pixbuf);//test in get_data_uri_by_path
+	char* get_data_uri_by_path(const char* path);//test ok
+	char* get_data_uri_by_pixbuf(GdkPixbuf* pixbuf);//test in get_data_uri_by_path //test ok
 	char* pixbuf_to_canvas_data(GdkPixbuf* pixbuf);
 
-#if 1
-    Test({
+#if 0
+	Test({
     	const char* path  = "/tmp/test_files/default_background.jpg";
+    	g_message("start");
         char* c = get_data_uri_by_path(path);
         // g_message("%s",c);
-        g_free(path);
         g_free(c);
+    	g_message("end");
+
     }, "get_data_uri_by_path");
 #endif
 
-#if 0
+#if 1
     Test({
-    	const char* path  = "/tmp/test_files/default_background.jpg";
+    	const char* path  = "/tmp/test_files/bdlogo.gif";
     	GError *error = NULL;
     	GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file(path, &error);
     	if (error != NULL) {
     	    g_warning("%s\n", error->message);
     	    g_error_free(error);
-    	    return NULL;
     	}
-        char* c = pixbuf_to_canvas_data(pixbuf);
-        g_message("%s",c);
-        g_free(path);
-        g_free(pixbuf);
-        g_free(c);
-
-    }, "get_data_uri_by_path");
+    	else{
+	    	char* c = pixbuf_to_canvas_data(pixbuf);
+	        // g_message("%s",c);
+	        g_free(c);
+    	}
+	    g_object_unref(pixbuf);
+    }, "pixbuf_to_canvas_data");
 #endif
 
     // tear_down_fixture();
