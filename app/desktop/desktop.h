c45bfea927ebacacb365aae31f3ee890a81b8f3d
bluth653426595 <ycl_pj@163.com>
2013-10-09 15:16:37 +0800

    app/desktop: add check_version && check_version_equal_set

---
+++ app/desktop/desktop.h
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2011 ~ 2013 Deepin, Inc.
+ *               2011 ~ 2013 bluth
+ *
+ * Author:      bluth <yuanchenglu@linuxdeepin.com>
+ * Maintainer:  bluth <yuanchenglu@linuxdeepin.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef DESKTOP_H
+#define DESKTOP_H
+
+#include "utils.h"
+
+#define DESKTOP_MAJOR_VERSION 0
+#define DESKTOP_MINOR_VERSION 0
+#define DESKTOP_SUBMINOR_VERSION 0
+#define DESKTOP_VERSION STR(DESKTOP_MAJOR_VERSION)"."STR(DESKTOP_MINOR_VERSION)"."STR(DESKTOP_SUBMINOR_VERSION)
+
+#endif /* end of include guard: DESKTOP_H */
+
