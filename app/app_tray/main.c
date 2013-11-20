03ed536dab9e013794515b53a539b08c35d3d440
kosl90 <kos1990l@gmail.com>
2013-11-20 16:15:08 +0800

    app:tray:: hide/show tray

--- app/app_tray/main.c
+++ app/app_tray/main.c
@@ -2,9 +2,22 @@
 
 #include "na-tray.h"
 
+#include "main.h"
+#include "tray_hide.h"
+#include "tray_guard_window.h"
 #include "X_misc.h"
 
 
+static GtkWidget* container = NULL;
+
+
+GdkWindow* TRAY_GDK_WINDOW()
+{
+    return gtk_widget_get_window(container);
+}
+
+
+static
 gboolean draw_background(GtkWidget* widget, cairo_t* cr, gpointer user_data)
 {
     GtkWidget* container = GTK_WIDGET(widget);
@@ -16,7 +29,7 @@ gboolean draw_background(GtkWidget* widget, cairo_t* cr, gpointer user_data)
     cairo_set_source_rgba(cr, 0, 0, 0, .8);
     GtkAllocation allocation;
     gtk_widget_get_allocation (container, &allocation);
-    cairo_rectangle (cr, allocation.x, allocation.y, allocation.width, allocation.height);
+    /* cairo_rectangle (cr, allocation.x, allocation.y, allocation.width, allocation.height); */
     cairo_fill(cr);
     cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
     cairo_paint(cr);
@@ -27,8 +40,28 @@ gboolean draw_background(GtkWidget* widget, cairo_t* cr, gpointer user_data)
     na_tray_force_redraw(tray);
 
     gtk_window_move(GTK_WINDOW(container), (gdk_screen_width() - allocation.width)/2.0, 0);
+    init_tray_guard_window(allocation.width);
+    update_tray_guard_window_position(allocation.width);
 
     return FALSE;
+}
+
+
+static
+gboolean leave_notify(GtkWidget* widget, GdkEvent* event, gpointer user_data)
+{
+    g_debug("[%s]", __func__);
+    if (!is_mouse_in_tray())
+        tray_delay_hide(100);
+    return FALSE;
+}
+
+
+static
+gboolean enter_notify(GtkWidget* widget, GdkEvent* event, gpointer user_data)
+{
+    g_debug("[%s]", __func__);
+    tray_show_now();
 }
 
 
@@ -36,7 +69,10 @@ int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
 
-    GtkWidget* container = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    if (argc == 2 && g_strcmp0(argv[1], "-d") == 0)
+        g_setenv("G_MESSAGES_DEBUG", "all", FALSE);
+
+    container = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     NaTray* tray = na_tray_new_for_screen(gdk_screen_get_default(), GTK_ORIENTATION_HORIZONTAL);
     gtk_container_add(GTK_CONTAINER(container), GTK_WIDGET(tray));
     gtk_widget_show(GTK_WIDGET(tray));
@@ -48,19 +84,23 @@ int main(int argc, char *argv[])
     gtk_widget_realize(GTK_WIDGET(tray));
 
     g_signal_connect(container, "draw", G_CALLBACK(draw_background), (gpointer)tray);
+    g_signal_connect(container, "leave-notify-event", G_CALLBACK(leave_notify), NULL);
+    g_signal_connect(container, "enter-notify-event", G_CALLBACK(enter_notify), NULL);
 
     GdkWindow* window = gtk_widget_get_window(container);
     set_wmspec_dock_hint(window);
 
     /* GdkColor c = {0,0,0,0}; */
     /* na_tray_set_colors(tray, &c, &c, &c, &c); */
-    gtk_widget_set_size_request(container, -1, 30);
+    gtk_widget_set_size_request(container, -1, TRAY_HEIGHT);
 
     GtkAllocation allocation;
     gtk_widget_get_allocation (container, &allocation);
     gtk_window_move(GTK_WINDOW(container), (gdk_screen_width() - allocation.width)/2.0, gdk_screen_height());
 
     gtk_widget_show_all(container);
+    na_tray_force_redraw(tray);
+    tray_delay_hide(1000);
 
     gtk_main();
     return 0;
---
+++ app/app_tray/main.h
@@ -0,0 +1,9 @@
+#ifndef __TRAY_H__
+#define __TRAY_H__
+
+#define TRAY_HEIGHT 30
+
+GdkWindow* TRAY_GDK_WINDOW();
+
+#endif /* end of include guard: __TRAY_H__ */
+
