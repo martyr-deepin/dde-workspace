0ac3279571b68534a4be72133edf3f0840274be1
snyh <snyh@snyh.org>
2014-05-22 15:15:27 +0800

    add guide app

---
+++ app/guide/guide.c
@@ -0,0 +1,61 @@
+#include <gtk/gtk.h>
+
+#include "X_misc.h"
+#include "jsextension.h"
+#include "dwebview.h"
+#include "i18n.h"
+#include "utils.h"
+
+#include "background.h"
+
+GtkWidget* get_container() 
+{
+    static GtkWidget* container = NULL;
+    if (container == NULL) {
+	container = create_web_container (FALSE, TRUE);
+    }
+    return container;
+}
+
+int main (int argc, char **argv)
+{
+    if (is_application_running("com.deepin.dde.guide")) {
+        g_warning("another instance of application dde-guide is running...\n");
+        return 0;
+    } else {
+	singleton("com.deepin.dde.guide");
+    }
+
+    init_i18n ();
+
+    gtk_init (&argc, &argv);
+
+    GtkWidget *webview = d_webview_new_with_uri (GET_HTML_PATH("guide"));
+    gtk_container_add (GTK_CONTAINER(get_container()), GTK_WIDGET (webview));
+    g_signal_connect(webview, "draw", G_CALLBACK(erase_background), NULL);
+
+    gtk_widget_realize (get_container());
+    gtk_widget_realize (webview);
+
+
+    {
+	void guide_disable_dock_region();
+	void guide_disable_right_click();
+	//TEST
+	guide_disable_dock_region();
+	guide_disable_right_click();
+    }
+
+    GdkWindow* gdkwindow = gtk_widget_get_window (get_container());
+    gdk_window_move_resize(gdkwindow, 0, 0, gdk_screen_width(), gdk_screen_height());
+
+    gdk_window_set_keep_above (gdkwindow, TRUE);
+    gdk_window_set_override_redirect (gdkwindow, TRUE);
+
+    gtk_widget_show_all (get_container());
+
+    gtk_main ();
+
+    return 0;
+}
+
---
+++ app/guide/helper.c
@@ -0,0 +1,43 @@
+#include <gtk/gtk.h>
+#include <X11/extensions/shape.h>
+#include "X_misc.h"
+
+GtkWidget* get_container();
+
+void guide_disable_right_click()
+{
+    Display* dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+    XGrabButton(dpy, Button3, AnyModifier, DefaultRootWindow(dpy), True, ButtonPressMask | ButtonReleaseMask, GrabModeAsync, GrabModeAsync, None, None);
+}
+void guide_enable_right_click()
+{
+    Display* dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+    XUngrabButton(dpy, Button3, AnyModifier, DefaultRootWindow(dpy));
+}
+void guide_disable_keyboard()
+{
+    Display* dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+    XGrabKeyboard(dpy, DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync, CurrentTime);
+}
+void guide_enable_keyboard()
+{
+    Display* dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+    XUngrabKeyboard(dpy, CurrentTime);
+}
+
+void guide_disable_dock_region()
+{
+    //TODO: find the dock XID
+    Window dock = 0x2a00004;
+
+    Display* dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
+    cairo_region_t* dock_region = get_window_input_region(dpy, dock);
+    gdk_window_input_shape_combine_region(gtk_widget_get_window(get_container()), dock_region, 0, 0);
+    cairo_region_destroy(dock_region);
+}
+
+void guide_enable_dock_region()
+{
+    gdk_window_input_shape_combine_region(gtk_widget_get_window(get_container()), NULL, 0, 0);
+}
+
