04d4b81f889a1a4cf5628cad414cf6e0eccb255e
bluth653426595 <ycl_pj@163.com>
2014-05-28 16:10:38 +0800

    add get_dock_xid

--- app/guide/helper.c
+++ app/guide/helper.c
@@ -25,12 +25,67 @@ void guide_enable_keyboard()
     XUngrabKeyboard(dpy, CurrentTime);
 }
 
+Window get_dock_xid()
+{
+    GDBusProxy *dock_proxy = NULL;
+    GError *error = NULL;
+    GVariant *dock_xid = NULL;
+
+    dock_proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+                                                G_DBUS_PROXY_FLAGS_NONE,
+                                                NULL,
+                                                "com.deepin.dde.dock",
+                                                "/com/deepin/dde/dock",
+                                                "com.deepin.dde.dock",
+                                                NULL,
+                                                &error);
+
+    if (error != NULL) {
+        g_warning ("get_dock_xid:dock proxy %s\n", error->message);
+        g_error_free (error);
+        g_object_unref (dock_proxy);
+        return 0;
+    }
+    error = NULL;
+
+    dock_xid = g_dbus_proxy_call_sync (dock_proxy,
+                            "Xid",
+                            g_variant_new("()"),
+                            G_DBUS_CALL_FLAGS_NONE,
+                            -1,
+                            NULL,
+                            &error);
+
+    if (error != NULL) {
+        g_warning ("get_dock_xid:Xid %s\n", error->message);
+        g_error_free (error);
+        g_object_unref (dock_proxy);
+        return 0;
+    }
+
+    if (dock_xid == NULL) {
+        g_warning ("get_dock_xid:Xid is NULL.\n");
+        g_error_free (error);
+        g_object_unref (dock_proxy);
+        return 0;
+    }
+
+    error = NULL;
+    g_object_unref (dock_proxy);
+    guint64 xid = 0;
+    g_variant_get(dock_xid, "(t)", &xid);
+    g_message("get_dock_xid:xid:%lu",xid);
+    return xid;
+}
+
 void guide_disable_dock_region()
 {
     //TODO: find the dock XID
-    Window dock = 0x2a00004;
-    return;
-
+    Window dock = get_dock_xid();
+    if(dock == 0){
+        g_warning("get_dock_xid error and return 0.");
+        return;
+    }
     Display* dpy = GDK_DISPLAY_XDISPLAY(gdk_display_get_default());
     cairo_region_t* dock_region = get_window_input_region(dpy, dock);
     gdk_window_input_shape_combine_region(gtk_widget_get_window(get_container()), dock_region, 0, 0);
