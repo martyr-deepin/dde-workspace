d5eb04846f70158169d3faf0f6e7df5189779934
snyh <snyh@snyh.org>
2014-06-09 20:56:08 +0800

    app:dock move launch dde_dock_applets to startdde

--- app/dock/dock.c
+++ app/dock/dock.c
@@ -343,7 +343,6 @@ void dock_emit_webview_ok()
         init_config();
         // update_dock_size_mode();
         init_dock_guard_window();
-        g_spawn_command_line_async("/usr/bin/dde-dock-applets", NULL);
     } else {
         update_dock_size_mode();
     }
