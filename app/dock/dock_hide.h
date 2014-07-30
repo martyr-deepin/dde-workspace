8784635eb677980b19a02f8cc98177e70468f418
liliqiang <kos1990l@gmail.com>
2014-07-30 11:31:17 +0800

    app:dock:: remove useless codes

--- app/dock/dock_hide.h
+++ app/dock/dock_hide.h
@@ -25,20 +25,9 @@
 
 #include <glib.h>
 
-void dock_delay_show(int delay);
-void dock_delay_hide(int delay);
-void dock_show_now();
-void dock_hide_now();
-void dock_hide_real_now();
-void dock_show_real_now();
-
-void dock_toggle_show();
-void dock_update_hide_mode();
-
-void update_dock_guard_window_position(double width);
-
 void init_dock_guard_window();
 gboolean is_mouse_in_dock();
+void update_dock_guard_window_position(double width);
 void cancel_update_state_request();
 #endif
 
